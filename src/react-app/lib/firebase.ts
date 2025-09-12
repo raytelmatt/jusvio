94bdc0f2907411d0ea066691d3601aed98a93fcf
Matthew Ray <iahmatt@Mac.attlocal.net>
2025-09-11 21:29:11 -0500

    fix(build): add firebase pkg and correct storage bucket

--- src/react-app/lib/firebase.ts
+++ src/react-app/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAb45jjLqzrnRYnqc5WlYvvKwYHZhxoU8g",
   authDomain: "jusivo.firebaseapp.com",
   projectId: "jusivo",
-  storageBucket: "jusivo.firebasestorage.app",
+  storageBucket: "jusivo.appspot.com",
   messagingSenderId: "829325582202",
   appId: "1:829325582202:web:07b6036fa03e2df73f40c3",
   measurementId: "G-1L2R9BGTM1"
